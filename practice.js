c5ba74175860f6abc1c6bba0db05d2f9745951c4
PavelMiit <pmmitchkin@gmail.com>
2022-07-07 20:35:50 +0300

    learning13

--- practice.js
+++ practice.js
@@ -192,3 +192,19 @@ const mySubstr = (text, number) => {
   }
   return result;
 };
+
+
+//Функция countChars() из теории считает, сколько раз входит буква в предложение и при этом учитывает 
+//регистр букв. То есть A и a с её точки зрения разные символы. Реализуйте вариант этой же функции, так 
+//чтобы регистр букв был не важен
+const countChars = (str, char) => {
+  let i = 0;
+  let count = 0;
+  while (i < str.length) {
+    if (str[i].toLowerCase() === char.toLowerCase()) {
+      count = count + 1;
+    }
+    i = i + 1;
+  }
+  return count;
+};
